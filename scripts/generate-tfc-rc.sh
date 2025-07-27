34cf9eef48a2a516b52857ae5e2f0be8da104563
sujay2306 <sujayks007@gmail.com>
2025-07-27 17:21:17 +0530

    destroyed all the resources

--- scripts/generate-tfc-rc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-cat << EOF > ~/.terraformrc
-credentials "app.terraform.io" {
-    token = "${TERRAFORM_CLOUD_TOKEN}"
-}
-EOF
--- scripts/get-short-sha.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -e
-echo '{"sha": "'"$(git rev-parse --short HEAD)"'"}'
